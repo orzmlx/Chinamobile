09f85e765be344217609a88e4cd3016ac936bbdb
orzmlx <43385093@qq.com>
2024-06-13 00:24:44 +0800

    china mobile

--- compare_kpi.py
+++ compare_kpi.py
@@ -14,6 +14,16 @@ def select_data_by_date(base_df, df, on_index, date_offset, date_index):
     copy_base_df = copy_base_df.merge(df, on=[on_index], how='left')
     copy_base_df[date_index] = pd.to_datetime(copy_base_df[date_index], format='%Y-%m-%d')
     copy_base_df['调整日期'] = pd.to_datetime(copy_base_df['调整日期'], format='%Y-%m-%d')
+    before_df = copy_base_df[copy_base_df[date_index] == copy_base_df['调整日期'] - pd.Timedelta(days=7)]
+    after_df = copy_base_df[copy_base_df[date_index] == copy_base_df['调整日期'] + pd.Timedelta(days=7)]
+    return before_df, after_df
+
+
+def select_data_by_date_range(base_df, df, on_index, date_offset, date_index):
+    copy_base_df = copy.deepcopy(base_df)
+    copy_base_df = copy_base_df.merge(df, on=[on_index], how='left')
+    copy_base_df[date_index] = pd.to_datetime(copy_base_df[date_index], format='%Y-%m-%d')
+    copy_base_df['调整日期'] = pd.to_datetime(copy_base_df['调整日期'], format='%Y-%m-%d')
     before_df = copy_base_df[
         copy_base_df[date_index].between(copy_base_df['调整日期'] - pd.Timedelta(days=7) - pd.Timedelta(days=date_offset),
                                          copy_base_df['调整日期'] - pd.Timedelta(days=7))]
@@ -23,7 +33,17 @@ def select_data_by_date(base_df, df, on_index, date_offset, date_index):
     return before_df, after_df
 
 
-def parse_kpi(base_df, files, use_cols, on_index, date_index, date_offset, out_path):
+def read_raw_date(path, filter_map, use_cols, on_index, date_index):
+    file_df = pd.read_csv(path, usecols=use_cols + [on_index, date_index], encoding='gbk')
+    if len(filter_map) == 0:
+        return file_df
+    for key, value in filter_map.items():
+        condition = key + '==' + value
+        file_df = file_df.eval(condition)
+    return file_df
+
+
+def parse_kpi(func, base_df, files, use_cols, on_index, date_index, date_offset, out_path, filter_map):
     before_df_list = []
     after_df_list = []
     file_number = len(files)
@@ -31,9 +51,9 @@ def parse_kpi(base_df, files, use_cols, on_index, date_index, date_offset, out_p
         base_df.rename(columns={'CGI': '小区CGI'}, inplace=True)
     for i in tqdm(range(file_number)):
         file = files[i]
-        file_df = pd.read_csv(file, usecols=use_cols + [on_index, date_index], encoding='gbk')
-        before_df, after_df = select_data_by_date(base_df, file_df[use_cols + [on_index, date_index]], on_index,
-                                                  date_offset, date_index)
+        file_df = read_raw_date(file, filter_map, use_cols, on_index, date_index)
+        # file_df = pd.read_csv(file, usecols=use_cols + [on_index, date_index], encoding='gbk')
+        before_df, after_df = func(base_df, file_df[use_cols + [on_index, date_index]], on_index, date_offset, date_index)
         before_df = get_avg(before_df, base_df, use_cols, on_index)
         after_df = get_avg(after_df, base_df, use_cols, on_index)
         before_df_list.append(before_df)
@@ -93,16 +113,13 @@ if __name__ == "__main__":
     on_1 = '小区CGI'
     date_col = '数据开始时间'
     date_col_1 = '开始时间'
-    # base_cols = ['数据开始时间', 'CGI']
-    # base_cols_1 = ['开始时间', '小区CGI']
     work_dir = 'C:\\Users\\orzmlx\\Desktop\\chinamobile\\ye'
     index_file_path = 'C:\\Users\\orzmlx\\Desktop\\chinamobile\\45G互操作策略调整小区清单汇总-0607 - 剔除3月前(1).xlsx'
     g4_performance_path = 'C:\\Users\\orzmlx\\Desktop\\chinamobile\\ye\\4G性能指标'
     g4_performance_cols = ['总流量(MB)(MB)']
     g5_performance_path = 'C:\\Users\\orzmlx\\Desktop\\chinamobile\\ye\\5G性能指标'
     g5_performance_cols = ['PDCP上行业务字节数(Mbyte)', 'PDCP下行业务字节数(Mbyte)', '小区UE上行速率小于1Mbps的样本数(个)',
-                           '小区UE上行速率低于1Mbps样本比例(%)',
-                           '小区UE下行速率小于5Mbps的样本数(个)', '小区UE下行速率低于5Mbps样本比例(%)']
+                           '小区UE上行速率低于1Mbps样本比例(%)', '小区UE下行速率小于5Mbps的样本数(个)', '小区UE下行速率低于5Mbps样本比例(%)']
 
     g4_performance_files = common_utils.find_file(g4_performance_path, '.csv')
     g5_performance_files = common_utils.find_file(g5_performance_path, '.csv')
@@ -111,5 +128,7 @@ if __name__ == "__main__":
     g5_performance_out_path = os.path.join(work_dir, '5G性能指标_summary.csv')
     g4_performance_out_path = os.path.join(work_dir, '4G性能指标_summary.csv')
 
-    parse_kpi(g5_base_df, g5_performance_files, g5_performance_cols, on, date_col, 3, g5_performance_out_path)
-    parse_kpi(g4_base_df, g4_performance_files, g4_performance_cols, on_1, date_col_1, 3, g4_performance_out_path)
+    parse_kpi(select_data_by_date_range, g5_base_df, g5_performance_files, g5_performance_cols, on, date_col, 3,
+              g5_performance_out_path, {})
+    parse_kpi(select_data_by_date_range, g4_base_df, g4_performance_files, g4_performance_cols, on_1, date_col_1, 3,
+              g4_performance_out_path, {})
